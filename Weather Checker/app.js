6809562f29f930a6e38d87992202482fc8b1817a
Metamorph Dev <42251514+gtonussi@users.noreply.github.com>
2020-01-30 18:57:00 -0300

    Add files via upload

---
+++ Weather Checker/app.js
@@ -0,0 +1,34 @@
+//Init storage
+const storage = new Storage();
+//get stored location data
+const weatherLocation = storage.getLocationData();
+//Init weather object
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+//Init UI
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', getWeather);
+
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+
+    storage.setLocationData(city, state);
+
+    //get and display weather
+    getWeather();
+
+    //close modal
+    $('#locModal').modal('hide');
+})
+
+function getWeather(){
+    weather.getWeather()
+    .then(results => {
+        ui.paint(results);
+    })
+    .catch(err => console.log(err));
+}
